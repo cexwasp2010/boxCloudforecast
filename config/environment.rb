80f039b09c23f0f457e6fffd2f2f76ec63d6f3fd
Cesar Córdoba <cexwasp2010@gmail.com>
2021-05-05 23:34:16 -0500

    Added aws_keys.rb to access to KMS credentials

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+aws_keys = File.join(Rails.root, 'config', 'aws_keys.rb')
+load(aws_keys) if File.exist?(aws_keys)
+
 # Initialize the Rails application.
 Rails.application.initialize!
